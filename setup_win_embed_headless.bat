0f0bdae566df23db51995861dc8e450c7f808f5a
FortunaCournot <65296393+FortunaCournot@users.noreply.github.com>
2025-11-24 18:11:23 +0100

    Update setup_win_embed_headless.bat

--- setup_win_embed_headless.bat
+++ setup_win_embed_headless.bat
@@ -11,7 +11,10 @@ python -m pip install wheel
 REM make sure we have setuptools available in the venv    
 python -m pip install --no-input -q setuptools
 
-python diag_setup_env.py
+python -c "import sys,runpy; sys.path.insert(0, r'%CD%'); runpy.run_path('diag_setup_env.py', run_name='main')"
 
-python -m setup.setup_windows_embedded --headless
+ECHO running setup ...
+python -c "import sys,os,runpy; sys.path.insert(0, r'%CD%'); runpy.run_module('setup.setup_windows_embedded', run_name='main')"
+ECHO done.
+
 
